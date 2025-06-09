23096af012b8a7bcb88b778a200dd386c75c3e2c
SamuelAlehegn <hadis.samuelalehegn@gmail.com>
2025-06-09 16:12:57 -0400

    fix: set default city to Paris if location is unavailable

--- WEATHER-APP/src/app/weather/weather.component.ts
+++ WEATHER-APP/src/app/weather/weather.component.ts
@@ -24,32 +24,31 @@ export class WeatherComponent implements OnInit {
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
-    if (navigator.geolocation) {
-      this.getLocationWeather();
-    } else {
-      this.setDefaultCityWeather();
-    }
+    this.getLocationWeather();
   }
 
   getWeatherData(): void {
     this.weatherService.getWeather(this.city, this.units).subscribe({
       next: (res: any) => {
-        console.log(res);
         this.weather = res;
-        this.temp = this.weather.main.temp;
-        this.feelsLikeTemp = this.weather.main.feels_like;
-        this.pressure = this.weather.main.pressure;
-        this.humidity = this.weather.main.humidity;
-        this.summary = this.weather.weather[0].main;
-
-        this.iconUrl =
-          'https://openweathermap.org/img/wn/' +
-          this.weather.weather[0].icon +
-          '@2x.png';
+        this.updateWeatherData();
       },
-      error: (error: { message: any }) => console.log(error.message),
-      complete: () => console.info('API call completed'),
+      error: (error: { message: any }) => console.error('Error fetching weather data:', error.message),
+      complete: () => console.info('Weather API call completed'),
     });
+  }
+
+  private updateWeatherData(): void {
+    if (this.weather && this.weather.main) {
+      this.temp = this.weather.main.temp;
+      this.feelsLikeTemp = this.weather.main.feels_like;
+      this.pressure = this.weather.main.pressure;
+      this.humidity = this.weather.main.humidity;
+      this.summary = this.weather.weather[0]?.main || '';
+      this.iconUrl = this.weather.weather[0]?.icon
+        ? `https://openweathermap.org/img/wn/${this.weather.weather[0].icon}@2x.png`
+        : '';
+    }
   }
 
   onSubmit(): void {
@@ -94,39 +93,47 @@ export class WeatherComponent implements OnInit {
   getLocationWeather() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        (position) => {
-          this.latitude = position.coords.latitude;
-          this.longitude = position.coords.longitude;
-          // this.getCityByCoordinates(); 
-
-          this.weatherService
-            .getCurrentWeather(this.latitude, this.longitude)
-            .subscribe({
-              next: (res: any) => {
-                console.log("location", res); 
-                this.inputCity = res[0].state;
-                this.city = this.inputCity;
-                this.getWeatherData();
-              },
-              error: (error: { message: any }) => console.log(error.message),
-              complete: () => console.info('API call completed'),
-            });
-          
-        },
-        (error) => {
-          console.log('Error occurred while retrieving location:', error);
-          this.setDefaultCityWeather();
-        }
+        this.handleLocationSuccess.bind(this),
+        this.handleLocationError.bind(this)
       );
     } else {
-      console.log('Geolocation is not supported by this browser.');
-      this.setDefaultCityWeather();
+      this.handleLocationError({
+        code: 0,
+        message: 'Geolocation is not supported by this browser.',
+        PERMISSION_DENIED: 1,
+        POSITION_UNAVAILABLE: 2,
+        TIMEOUT: 3
+      });
     }
   }
 
-  setDefaultCityWeather() {
-    this.city = 'paris'; // Default city if the user does not grant access to their location
-    this.inputCity = 'paris';
+  private handleLocationSuccess(position: GeolocationPosition) {
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+    
+    this.weatherService.getCurrentWeather(this.latitude, this.longitude).subscribe({
+      next: (res: any) => {
+        if (res && res.length > 0) {
+          this.inputCity = res[0].state || res[0].name;
+          this.city = this.inputCity;
+          this.getWeatherData();
+        } else {
+          this.handleLocationError({ code: 0, message: 'Unable to determine location.' } as GeolocationPositionError);
+        }
+      },
+      error: (error: { message: any }) => this.handleLocationError(error.message),
+      complete: () => console.info('Location API call completed'),
+    });
+  }
+
+  private handleLocationError(error: GeolocationPositionError): void {
+    // Handle the error here
+    console.error('Geolocation error:', error.message);
+  }
+
+  private setDefaultCityWeather() {
+    this.city = 'Paris';
+    this.inputCity = 'Paris';
     this.getWeatherData();
   }
 }
